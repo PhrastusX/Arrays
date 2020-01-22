80b7cd1ed38275c81fda9097bff6bbe833bc2f60
Theo Diehl <theo@Theos-MacBook-Pro.local>
2020-01-22 16:45:02 -0700

    added the code...

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+const array = ['theo', 'asheigh','carmen','jerry', 'jason', 'derin']
+
+console.log(array.length) //shows the length
+console.log(array[0])// at that index
+console.log(array[1])
+array.push('FM') // adds FM to end of array
+array.pop() //removes last item in array and returns it
+array.shift()//removes first item in array
+array.unshift('theophrastus')//adds items to beginning of array
+array.includes('jason') //returns true or false
+a = array.join()//makes the array a string
+console.log(a)
+array.reverse //reverses the order of the array
+array.sort()//default is alphabetical acending but you can pass a sorting function through it.
