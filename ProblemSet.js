c5d0a3a4411b687418630567863eb0b60592aae6
Theo Diehl <theo@Theos-MacBook-Pro.local>
2020-01-28 09:29:25 -0700

    start of problem set

---
+++ ProblemSet.js
@@ -0,0 +1,9 @@
+// Create an array with the names of different types of fruit and save it to a variable
+
+// Write code to access the third item in the fruit array and save it to a new variable.
+
+// Overwrite the fruit at the last position in the array with a new string. It can be anything you want.
+
+// Be sure to test your code is doing what you think it is doing by console.log() ing throughout.
+
+const newArray = ["banana", "peach", ]
